0f121c76d7122d3ac77143d530ae3e13a9c7af93
AlexisJml <96110187+AlexisJml@users.noreply.github.com>
2024-11-25 14:24:21 +0100

    Init C# project + init Calculatrice class with method

---
+++ Calculatrice.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace calculatrice
+{
+    class Calculatrice
+    {
+        public double Addition(double n1, double n2)
+        {
+            return n1 + n2;
+        }
+
+        public double Soustraction(double n1, double n2)
+        {
+            return n1 - n2;
+        }
+
+        public double Multiplication(double n1, double n2)
+        {
+            return n1 * n2;
+        }
+
+        public double Division(double n1, double n2)
+        {
+            if(n2 == 0)
+            {
+                return 0;
+            }else
+            {
+                return n1 / n2;
+            }
+        }
+    }
+}
