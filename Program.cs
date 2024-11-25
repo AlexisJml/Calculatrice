fc0da6d1fef1060ef365ea62270599bf45aecfca
AlexisJml <96110187+AlexisJml@users.noreply.github.com>
2024-11-25 14:35:19 +0100

    Using Calculator class in main

--- Program.cs
+++ Program.cs
@@ -4,9 +4,41 @@ namespace calculatrice
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Hello World!");
+            string operateur;
+            double n1, n2, result;
+            Calculatrice calc = new();
+
+            Console.WriteLine("Veuillez saisir l'opération souhaité (+, -, * ou /):");
+            operateur = Console.ReadLine();
+
+            Console.WriteLine("Veuillez saisir le premier nombre:");
+            n1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Veuillez saisir le second nombre:");
+            n2 = int.Parse(Console.ReadLine());
+
+            switch(operateur)
+            {
+                case "+":
+                    result = calc.Addition(n1, n2);
+                    break;
+                case "-":
+                    result = calc.Soustraction(n1, n2);
+                    break;
+                case "*":
+                    result = calc.Multiplication(n1, n2);
+                    break;
+                case "/":
+                    result = calc.Division(n1, n2);
+                    break;
+                default:
+                    result = 0;
+                    break;
+            }
+
+            Console.WriteLine("Le résultat de l'opération est " + result);
         }
     }
 }
